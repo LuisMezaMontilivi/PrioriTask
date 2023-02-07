f234e005840832982af79f8c41e4f518b8b286e6
jsotocMontilivi <a2106910@institutmontilivi.cat>
2023-02-07 19:05:04 +0100

    Actualitzacio Crud Usuari: login+canviContrasenya

--- api/BdD.php
+++ api/BdD.php
@@ -4,8 +4,17 @@ class BdD {
     private static $connection;
 
 	//La base de dades està posada de manera fixa al codi
+        //Per treballar en local cal canviar user i contra. Per a realease:
+            //  self::$connection = new PDO("mysql:host=127.0.0.1;dbname=PrioriTaskBD", "adminer", "Bhun@89ble.oient");
     public static function connect() {
-        self::$connection = new PDO("mysql:host=127.0.0.1;dbname=PrioriTaskBD", "adminer", "Bhun@89ble.oient");
+        try{
+            self::$connection = new PDO("mysql:host=localhost;dbname=PrioriTaskBD", "usuari", "1234");
+        }
+        catch(PDOException $e){
+           self::$connection = null;
+            return $e->getMessage();
+        }
+      
     }
 
 	//Tancar conexió a la BdD
@@ -15,6 +24,58 @@ class BdD {
 	
 	//Funcionalitats a la BdD
 
-}
+    public static function loginBD($email, $contrasenya){
+        $output = false;
+        try{
+            $query = (self::$connection)->prepare(
+                "
+                SELECT  * from Usuari u 
+                where email = :email && contrasenya = :contrasenya
+                ;
+                "
+            );
+            $query->bindParam(':email', $email);
+            $query->bindParam(':contrasenya', $contrasenya);
+            $query->execute();
+            $query->setFetchMode(PDO::FETCH_ASSOC);
+            $outcome = $query->fetchAll();
+            if(count($outcome)>0){
+                $output[0] = $outcome[0]["id_usuari"];
+                $output[1] = $outcome[0]["email"];
+            }
+        }
+        catch(PDOException $e) {
+            echo "Error: " . $e->getMessage();
+            $output = $e->getMessage();
+        }
+        
 
+        return $output;
+    }
+
+
+    public static function canviContrasenyaBD($token, $contrasenya){
+        $output = false;
+        try{
+            $query = (self::$connection)->prepare(
+                "
+                Update Usuari 
+                set contrasenya = :contrasenya
+                WHERE token = :token
+                ;
+                "
+            );
+            $query->bindParam(':contrasenya', $contrasenya);
+            $query->bindParam(':token', $token);
+            $outcome = $query->execute();
+            $output =  $query->rowCount();
+           
+         }
+         catch(PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+        return $output;
+
+    }
+}
 ?>
--- api/serve.php
+++ api/serve.php
@@ -4,8 +4,21 @@ require 'BdD.php';
 class Server {
 
     public function serve() {
-      
-        echo "Pastanaga";
+      $uri = $_SERVER['REQUEST_URI'];
+      $method = $_SERVER['REQUEST_METHOD'];
+        //echo $uri;
+
+        if($method == 'PUT'){
+          if($uri == '/API/usuari/iniciar'){
+            echo $this->login();
+          }
+          if($uri == '/API/usuari/contrasenya'){
+            echo $this->canviaContrasenya();
+          }
+        }
+        else{
+          echo "El mètode no era Put";
+        }
       
     }
     
@@ -14,7 +27,88 @@ class Server {
       return $uri['path'];
     }
 
-  }
+    /* Function: login
+
+    A partir dels Headers rebuts, validació de token inicial, login de l'usuari contra la BdD, 
+    creació del nou token a partir de les dades del usuari + token prèvi, inserció de nou token a la BdD, inserció de hora de la darrera petició de l'usuari a la BdD
+
+    Returns: boolea false si el procés no és fructífer/ array amb id,mail + headers corresponents: 200 OK o 401 Unauthorised
+
+    */
+    private function login(){
+      $email = $_SERVER['HTTP_EMAIL'];
+      $contrasenya = $_SERVER['HTTP_CONTRASENYA'];
+      $token = $_SERVER['HTTP_TOKEN']; //Obtenim el token per headers
+         //Funció de validació
+         if($token){
+       
+          BdD::connect();
+          $output = BdD::loginBD($email, $contrasenya);
+          if($output != false){
+            //Funció de generació i insert del nou token a la BDD. El resultat d'aquesta inserció serà el nostre output
+            //Funció de inserió de darrera petició a la BdD
+            
+            header('HTTP/1.1 200 OK');
+            }
+          else{
+            header('HTTP/1.1 401 Unauthorised');
+            $output= "no és usuari correcte";
+            }
+          // Tanquem la bdd
+          BdD::close();
+         }
+          return $output;
+        }
+
+
+        /* Function: canviaContrasenya(){
+          
+          A partir dels Headers rebuts, validació de token rebut, canvi de contrassenya a la BdD al usuari que té el token, retorn d'OK o Unauthorised
+
+           Returns: boolea resultant del procés + headers corresponents: 200 OK o 401 Unauthorised
+
+        */
+        private function canviaContrasenya(){
+          $token = $_SERVER['HTTP_TOKEN']; //Obtenim el token per headers
+          $contrasenya = $_SERVER['HTTP_CONTRASENYA'];
+          echo "Ha entrat a la funció";
+          //Validació del token actual
+          if($token)
+          {
+          BdD::connect();
+          echo "Ha entrat a token i ha conectat";
+            $output = BdD::canviContrasenyaBD($token, $contrasenya);
+            if($output == 1)
+            {
+              //Funció de inserió de darrera petició a la BdD
+              header('HTTP/1.1 200 OK');
+              }
+              else{
+                //  header('HTTP/1.1 304 Not Modified'); //Preguntar a Carles
+                $output = "Token Incorrecte";
+                }
+               // Tanquem la bdd
+            BdD::close();
+          } 
+          else{
+            header('HTTP/1.1 401 Unauthorised');
+            $output = "Token Incorrecte";
+            }
+          echo $output;
+          return $output;
+
+
+        }
+
+
+
+
+
+    
+
+    }
+
+  
 
 
 
