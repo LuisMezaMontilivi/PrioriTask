7ee830e9e081f2a957524989976ec9e6d457c095
Luis Meza <a2106917@institutmontilivi.cat>
2023-02-21 17:15:45 +0100

    Plantilla avís legal

--- front/src/components/FooterComp.vue
+++ front/src/components/FooterComp.vue
@@ -1,8 +1,8 @@
 <template>
     <v-app-bar flat location="bottom"  color="secondary">
       <v-row justify="center" no-gutters>
-      <v-col class="text-left mt-4 ml-1" min-width="75px" cols="5">
-        <div ><strong>Informació legal i Ús de cookies</strong></div>
+      <v-col class="text-left mt-4 ml-1 info" min-width="75px" cols="5">
+        <div @click="reroute()"><strong>Informació legal i Ús de cookies</strong></div>
        
       </v-col>
       <v-col class="text-right mt-4" min-width="75px"  cols="5">
@@ -17,6 +17,17 @@
 </template>
 
 <script>
-
+export default {
+  methods: {
+    reroute(){
+        this.$router.push('/about');
+    }
+  }
+}
 </script>
 
+<style scoped>
+  .info{
+    cursor: pointer;
+  }
+</style>
--- front/src/views/AboutView.vue
+++ front/src/views/AboutView.vue
@@ -1,5 +1,26 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-sheet width="90%" class="mx-auto my-4">
+    <div class="about">
+      <h1>Informació legal i ús de cookies</h1>
+      <div>
+        A través d'aquest lloc web no es demanen dades de caràcter personal de les persones usuàries sense el seu coneixement, ni es cedeixen a tercers.
+        <br/><br/>
+        Amb la finalitat d'oferir-vos el millor servei i amb l'objecte de facilitar-ne l'ús, s'analitzen el nombre de pàgines visitades, el nombre de visites, així com l'activitat de les persones visitants i la seva freqüència d'utilització. A aquest efecte, l‟Agència Espanyola de Protecció de Dades (PrioriTask) utilitza la informació estadística elaborada pel Proveïdor de Serveis d‟Internet.
+        <br/><br/>
+        PrioriTask no utilitza galetes per recollir informació de les persones usuàries, ni registra les adreces IP d'accés. Únicament s'utilitzen cookies pròpies, de sessió, amb finalitat tècnica (aquelles que permeten la navegació a través del lloc web i la utilització de les diferents opcions i serveis que hi existeixen).
+        <br/><br/>
+        El portal del qual és titular PrioriTask conté enllaços a llocs web de tercers, les polítiques de privadesa dels quals són alienes a la de PrioriTask. En accedir a aquests llocs web vostè pot decidir si accepta les seves polítiques de privadesa i de cookies. Amb caràcter general, si navega per internet vostè pot acceptar o rebutjar les galetes de tercers des de les opcions de configuració del seu navegador.
+      </div>
+    </div>
+  </v-sheet>
 </template>
+
+<style scoped>
+  div{
+    font-size: 20px;
+    text-align: center;
+  }
+  br{
+    padding-bottom: 10px;
+  }
+</style>
